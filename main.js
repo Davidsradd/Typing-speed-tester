1b882656bf5776f2db60abde26b6284a1dd367b7
Davidsradd <57205283+Davidsradd@users.noreply.github.com>
2020-04-26 15:48:42 -0500

    resubmission for assignment
    i fixed the problem

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const originText = document.querySelector("#origin-text p").innerHTML;
 const resetButton = document.querySelector("#reset");
 const theTimer = document.querySelector(".timer");
 
+
 var timer = [0,0,0,0];
 var interval;
 var timerRunning = false;
